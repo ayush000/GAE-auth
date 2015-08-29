297d2e38b661fe65553c9535fdac451bfa531709
ayush <ayush000@gmail.com>
2015-08-29 19:24:24 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import webapp2
+import jinja2
+import time
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(autoescape=True, extensions=['jinja2.ext.autoescape'],
+                               loader=jinja2.FileSystemLoader(template_dir))
+
+
+# css_dir=os.path.join(os.path.dirname(__file__), "stylesheets")
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainHandler(Handler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        visits = self.request.cookies.get('visits', '0')
+        if visits.isdigit():
+            visits=int(visits)+1
+        else:
+            visits=0
+
+        self.response.headers.add_header('Set-Cookie','visits={}'.format(visits))
+        if visits>20:
+            self.write("You are the best")
+        else:
+            self.write("you've been there {} times".format(visits))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
